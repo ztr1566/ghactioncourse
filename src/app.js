5c90882c1cd9df03f06a9bbffd735d93fb259043
Ziad T. Rashid <ztr1566@gmail.com>
2026-07-19 11:39:01 +0300

    refactor: comment out main execution block in app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ function greet(name) {
 
 module.exports = greet;
 
-if (require.main === module) {
-    console.log(greet("World"));
-}
+// if (require.main === module) {
+//     console.log(greet("World"));
+// }
